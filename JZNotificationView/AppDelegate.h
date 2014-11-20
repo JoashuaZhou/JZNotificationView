1c754bc78cddde100314f3e902934a26c8fa79b4
a226035102 <comeonjiji@gmail.com>
2014-11-20 17:19:26 +0800

    Initial Commit

---
+++ JZNotificationView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JZNotificationView
+//
+//  Created by Joshua Zhou on 14/11/20.
+//  Copyright (c) 2014年 Joshua Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
